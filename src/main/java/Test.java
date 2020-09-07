f248a668c3fe8aebfaa95c348ae24a1a53db76b1
azemedkun-git <azemedkun@miu.edu>
2020-09-07 00:28:05 -0500

    add a docker file with correct again updated

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello: this is first time to push from intellij to try");
+        System.out.println("Hello: this is first time to push from intellij to tr");
     }
 
 }
